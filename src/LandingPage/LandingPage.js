dafd5768c491f10aae94ff90c30fe1c6b8bbdb14
Tatia burdett <tatiaburdett@gmail.com>
2021-03-25 14:07:50 -0700

    simple styling, positioning

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -7,10 +7,12 @@ class LandingPage extends React.Component {
   render() {
     return (
       <div>
-        <header>
-          <h1>Hello World</h1>
-          <h2>An anonymous group journal. Share openly.</h2>
-          <EnterButton />
+        <header className='landing-header'>
+          <div className='landing-header-content'>
+            <h1>Hello World</h1>
+            <h2>An anonymous group journal. Share openly.</h2>
+            <EnterButton />
+          </div>
         </header>
         <section>
 
