5013dfdd4a061e9fbe1b137a62d56bdf587396cc
Tatia burdett <tatiaburdett@gmail.com>
2021-03-25 12:00:35 -0700

    install Router, add to index

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import DATA from './data'
 
 class App extends React.Component {
+  
+
   render() {
+    console.log(DATA[0].nickname)
     return (
       <div>
-        <h1>Hello!</h1>
+        <h1>Hello World</h1>
       </div>
     )
   }
