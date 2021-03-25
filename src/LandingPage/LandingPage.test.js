052d5e43a2b5c2c73b1fba858cdc9e6490cb5334
Tatia burdett <tatiaburdett@gmail.com>
2021-03-25 12:39:10 -0700

    add CSS page and create test

--- src/LandingPage/LandingPage.test.js
+++ src/LandingPage/LandingPage.test.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import LandingPage from './LandingPage'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(
     <BrowserRouter>
-      <App />
-    </BrowserRouter>, 
-    div);
+      <LandingPage />
+    </BrowserRouter>,
+    div)
   ReactDOM.unmountComponentAtNode(div)
-});
+})
