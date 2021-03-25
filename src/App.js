fea99f206668af108c88b7518aec3c0b0db738f4
Tatia burdett <tatiaburdett@gmail.com>
2021-03-25 12:04:38 -0700

    set up app.test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import {Route, Link} from 'react-router-dom'
 import DATA from './data'
 
 class App extends React.Component {
-  
 
   render() {
     console.log(DATA[0].nickname)
