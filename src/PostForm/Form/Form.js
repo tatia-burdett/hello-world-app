c6f3dcf036c692a4ee2769119d40f9d178127ade
Tatia burdett <tatiaburdett@gmail.com>
2021-03-25 17:27:35 -0700

    add basic styling to form

--- src/PostForm/Form/Form.js
+++ src/PostForm/Form/Form.js
@@ -4,8 +4,23 @@ import './Form.css'
 class Form extends React.Component {
   render() {
     return (
-      <div>
-        Hello Form
+      <div className='form-field'>
+        <form>
+          <fieldset>
+            <legend>Create a Post!</legend>
+
+            <label for='nickname'>Name or Nickname:</label>
+            <input type='text' id='nickname' name='user' placeholder='Dr. Who' required/>
+
+            <label for='location'>Location:</label>
+            <input type='text' id='location' name='user' placeholder='Gallifrey'/>
+
+            <label for='content'>Say something:</label>
+            <textarea id='content' name='user' required/>
+
+            <button className='submit-btn'>Submit</button>
+          </fieldset>
+        </form>
       </div>
     )
   }
--- src/PostForm/PostForm.js
+++ src/PostForm/PostForm.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import './PostForm.css'
 
+import Form from './Form/Form'
+
 class PostForm extends React.Component {
   render() {
     return (
-      <div>
-        <header>
+      <div className='post-form'>
+        <header className='post-form-header'>
           <h1>Hello World</h1>
-          <h2>Say hello!</h2>
+          <h2>Share something!</h2>
         </header>
-        <section>
-          
+        <section className='post-form-form'>
+          <Form />
         </section>
       </div>
     )
