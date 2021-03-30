8c27f9cb434c2128f49510bea8423a6cf96cc000
Tatia burdett <tatiaburdett@gmail.com>
2021-03-30 10:08:36 -0700

    add onChange handlers

--- src/PostForm/Form/Form.js
+++ src/PostForm/Form/Form.js
@@ -2,6 +2,46 @@ import React from 'react'
 import './Form.css'
 
 class Form extends React.Component {
+  // button on submit, updates state
+  constructor(props) {
+    super(props)
+    this.state = {
+      nickname: {
+        value: ''
+      },
+      user_location: {
+        value: ''
+      },
+      content: {
+        value: ''
+      }
+    }
+  }
+
+  updateName = (nickname) => {
+    this.setState({
+      nickname: {
+        value: nickname
+      }
+    })
+  }
+
+  updateLocation = (user_location) => {
+    this.setState({
+      value: user_location
+    })
+  }
+
+  updateContent = (content) => {
+    this.setState({
+      value: content
+    })
+  }
+
+  handleSubmit = () => {
+
+  }
+
   render() {
     return (
       <div className='post-form-field'>
@@ -10,13 +50,32 @@ class Form extends React.Component {
             <legend>Create a Post!</legend>
 
             <label htmlFor='nickname'>Name or Nickname:</label>
-            <input type='text' id='nickname' name='user' placeholder='Dr. Who' required/>
+            <input 
+              type='text' 
+              id='nickname' 
+              name='user' 
+              placeholder='Dr. Who' 
+              onChange={e => this.updateName(e.target.value)}
+              required
+            />
 
             <label htmlFor='location'>Location:</label>
-            <input type='text' id='location' name='user' placeholder='Gallifrey'/>
+            <input 
+              type='text' 
+              id='location' 
+              name='user' 
+              placeholder='Gallifrey'
+              onChange={e => this.updateLocation(e.target.value)}
+            />
 
             <label htmlFor='content'>Say something:</label>
-            <textarea id='content' name='user' required/>
+            <textarea 
+              type='text'
+              id='content' 
+              name='user' 
+              onChange={e => this.updateContent(e.target.value)}
+              required
+            />
 
             <button className='submit-btn'>Submit</button>
           </fieldset>
