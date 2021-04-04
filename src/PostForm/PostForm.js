84a114ac4aa9e1162d5bce4db8b0aeb9a7cdf1c0
Tatia burdett <tatiaburdett@gmail.com>
2021-04-04 15:05:53 -0700

    resize textarea, remove h1

--- src/PostForm/PostForm.js
+++ src/PostForm/PostForm.js
@@ -8,7 +8,6 @@ class PostForm extends React.Component {
     return (
       <div className='post-form'>
         <header className='post-form-header'>
-          <h1>Hello World</h1>
           <h2>Share something!</h2>
         </header>
         <section className='post-form-form'>
