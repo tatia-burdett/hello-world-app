449a10b98ce38465dad6a699b3ef5766cfa19bcc
Tatia burdett <tatiaburdett@gmail.com>
2021-04-04 14:03:22 -0700

    quote styling - background color

--- src/LandingPage/Sections/Sections.js
+++ src/LandingPage/Sections/Sections.js
@@ -8,12 +8,13 @@ class Sections extends React.Component {
         <section className='landing-sections'>
           <h3 className='landing-sections-header'>Anonymously share your thoughts, frustrations, and successes!</h3>
           <div className='landing-sections-content'>
-            <p>Share something to the world and filter through posts. Categorize your post as an Expression, Release, Rejoice or Embrace. Express something about yourself, like an idea or thought, or maybe just say hi! Release some negative thought that's hanging over your head. Rejoice in a recent win or success! Or maybe send a warm embrace to someone who needs it.</p>
+            <p>Share something to the world and filter through posts.</p> <p>Categorize your post as an Expression, Release, Rejoice or Embrace. Express something about yourself, like an idea or thought, or maybe just say hi! Release some negative thought that's hanging over your head. Rejoice in a recent win or success! Or maybe send a warm embrace to someone who needs it.</p>
+            <hr/>
             <p>This project was inspired by a book that I used to pass around with my friends in high school. We didn't share very many classes, but we wanted to stay connected. We kept a large, empty journal, that we would pass around between us. When it was your turn to use the journal, you could write anything you wanted, and also catch up on what everyone else was going through. This journal brought us closer together, and I hope this online journal can do something similarily.</p>
           </div>
         </section>
         <section className='landing-sections'>
-          <h3 className='landing-sections-header'>Shrink the world, grow closer</h3>
+          <h3 className='landing-sections-header parallex-section-header'>Shrink the world, grow closer</h3>
           <div className='landing-sections-content'>
             <blockquote>
               “We are like islands in the sea, separate on the surface but connected in the deep.”
