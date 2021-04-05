4635b51f25de4482849b2705fd51e138616a5ec2
Tatia burdett <tatiaburdett@gmail.com>
2021-04-05 13:13:14 -0700

    add background to fix contrast issues

--- src/LandingPage/EnterButton/EnterButton.js
+++ src/LandingPage/EnterButton/EnterButton.js
@@ -5,7 +5,7 @@ import './EnterButton.css'
 class EnterButton extends React.Component {
   render() {
     return (
-      <div>
+      <div className='enter-button'>
         <Link to='/posts'>
           <p>View Posts</p>
         </Link>
