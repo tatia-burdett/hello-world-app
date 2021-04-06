18d09d10ad7b7ad1cbdced21bc65805c6e03091c
Tatia burdett <tatiaburdett@gmail.com>
2021-04-06 11:29:34 -0700

    add class to moment

--- src/SinglePost/SinglePost.js
+++ src/SinglePost/SinglePost.js
@@ -27,7 +27,7 @@ class SinglePost extends React.Component {
         <section className='single-post-container'>
           <h2 className='sinle-nickname'>{singleComment.nickname}</h2>
           <p className='single-location'>{singleComment.user_location}</p>
-          <Moment format='MMM D, YYYY'>{singleComment.date_posted}</Moment>
+          <Moment format='MMM D, YYYY' className='moment-time'>{singleComment.date_posted}</Moment>
           <p className='single-category'>{singleComment.category}</p>
           <p className='single-content'>{singleComment.content}</p>
           <button onClick={() => this.props.history.goBack()}>
